6caf4589acc9518fc2a5b851a872dfc35dfcff0f
ankit-tailor <tailorankit3345@gmail.com>
2021-04-10 22:48:40 +0530

    fix translation

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ let submitButton = document.querySelector('.submit-btn')
 let userInput = document.querySelector('#user-input')
 let minionTranslate = document.querySelector('#minion-translate')
 
-// console.log(submitButton, userInput, minionTranslate)
-
 let url = 'https://api.funtranslations.com/translate/minion.json?text='
 
 function genrateUrl(text) {
@@ -17,12 +15,12 @@ function handelError(error) {
 submitButton.addEventListener('click', () => {
   let textForTranslation = userInput.value
   let endpoint = genrateUrl(textForTranslation)
-  console.log(endpoint)
+
   fetch(endpoint)
     .then((response) => response.json())
     .then((json) => {
       minionTranslate.innerHTML = json.contents.translated
-      console.log(json)
+      //   console.log(json)
     })
     .catch((err) => handelError(err))
 })
